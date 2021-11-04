7f219158429610877d72ca043be2bbf24ef57378
Abner <zhongji.huang@hand-china.com>
2021-11-04 18:07:56 +0800

    feat: 更改基础配置

--- .umirc.ts
+++ .umirc.ts
@@ -2,20 +2,21 @@ const path = require('path');
 import { defineConfig } from 'dumi';
 
 const repo = 'hand-g4.gihub.io';
+const basePath = `/${repo}/`;
+const isProduction = process.env.NODE_ENV === 'production';
+const logoUrl = (isProduction ? basePath : '/') + 'hand-logo.png';
 
 export default defineConfig({
   // 详细配置: https://d.umijs.org/config
   title: '前端四组',
-  favicon: '/hand-logo.png', // 默认会指向项目根目录 /public
-  logo: '/hand-logo.png', // 默认会指向项目根目录 /public
+  favicon: logoUrl,
+  logo: logoUrl,
   outputPath: 'docs-dist',
   mode: 'site',
-  base: `/${repo}/`,
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: basePath,
+  publicPath: isProduction ? basePath : '/',
   styles: [`.__dumi-default-navbar-logo { background-size: 30% !important; }`],
-  resolve: {
-    passivePreview: true,
-  },
+  resolve: { passivePreview: true },
   alias: {
     '@/': path.resolve(__dirname, './src/'),
   },
