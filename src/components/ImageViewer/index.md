ea17363e473e36556117ef52346a2380f2d2cbb9
linyun <hly.baifeng@qq.com>
2021-11-07 21:09:40 +0800

    refactor: 清理无关内容

--- src/components/ImageViewer/index.md
+++
@@ -1,8 +0,0 @@
----
-title: ImageViewer
-order: 3
----
-
-## Demo
-
-<code src="./demos/Snapshot/Snapshot.tsx"></code>
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export * from './components/DraggableLayout';
 
-export * from './components/ImageViewer';
-
 export * from './utils/BlobResponseHelper';
 
 export * from './utils/FractionNumber';
